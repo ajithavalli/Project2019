578193a5cf63b5aab89bbf1f97839e24eec981fe
Ajitha Chellappan <ajithavalli.chellappan@thermofisher.com>
2019-09-27 23:14:24 +0530

    PCA checkin 1

--- CommonUniqueVariants.py
+++ CommonUniqueVariants.py
@@ -8,12 +8,15 @@ import pandas as pd
 
 vcf_variants = defaultdict(set)
 vcf_positions = defaultdict(set)
-
+variant_features = defaultdict(dict)
 
 def parseVCF(vcffile):
 	vcfreader = vcf.Reader(open(vcffile))
 	samples = vcfreader.samples
 	sampleName = None
+	features = ['AF', 'AC', 'DP', 'ExcessHet','FS','MQ','QD',
+	'GQ', 'DP', 'QUAL']
+
 	if len(samples) == 0:
 		print("No sample found, consolidating it as known_mutations")
 		sampleName = vcffile.split("/")[-1].replace(".vcf", "") + "_known_mutations"
@@ -26,8 +29,17 @@ def parseVCF(vcffile):
 				vcf_positions[sampleName].add(chrm + "_" + str(record.POS))
 				alts = record.ALT
 				for alt in alts:
-					vcf_variants[sampleName].add("_".join([chrm, str(record.POS), record.REF, str(alt)]))
-
+					keyh = "_".join([chrm, str(record.POS), record.REF, str(alt)])
+					vcf_variants[sampleName].add(keyh)
+					for field in features:
+						if field in ('AC', 'AF'):
+							variant_features[sampleName][keyh][field] = record.INFO[field][0]
+						elif field in ('DP', 'ExcessHet','FS','MQ','QD'):
+						    variant_features[sampleName][keyh][field] = record.INFO[field]
+						elif field in ('QUAL'):
+							variant_features[sampleName][keyh][field] = record.__getattribute__(field)
+						else:
+							print(field + " not found for " + keyh)
 		else:
 			for sample in samples:
 				sampleName = vcffile.split("/")[-1].replace(".vcf", "") + "_" + sample
@@ -39,7 +51,21 @@ def parseVCF(vcffile):
 				if len(alts) == 1:
 					alts = record.genotype(sample).gt_bases.split("/")
 				for alt in alts:
-					vcf_variants[sampleName].add("_".join([chrm, str(record.POS), record.REF, str(alt)]))
+					keyh = "_".join([chrm, str(record.POS), record.REF, str(alt)])
+					vcf_variants[sampleName].add(keyh)
+					variant_features[sampleName][keyh] = {}
+					for field in features:
+						if field in ('AF', 'AC'):
+							variant_features[sampleName][keyh][field] = record.INFO[field][0]
+						elif field in ('ExcessHet','FS','MQ','QD') and field in record.INFO:
+						    variant_features[sampleName][keyh][field] = record.INFO[field]
+						elif field in ('GQ', 'DP'):
+						    variant_features[sampleName][keyh][field] = record.genotype(sample)[field]
+						elif field in ('QUAL'):
+							variant_features[sampleName][keyh][field] = record.__getattribute__(field)
+						else:
+							print(field + " not found for " + keyh)
+
 
 def parseCosmic(cosmicVCF):
 	vcfreader = vcf.Reader(open(cosmicVCF))
@@ -68,9 +94,9 @@ def common_mutations(vcf_positions, vcf_variants, cosmicMutations):
 	## Common across all samples:
 	for sample in samples:
 		if allCommon == set():
-			allCommon = vcf_positions[sample]
+			allCommon = vcf_variants[sample]
 		else:
-			allCommon = allCommon.intersection(vcf_positions[sample])
+			allCommon = allCommon.intersection(vcf_variants[sample])
 	print("Common Across all samples:", allCommon)
 
 	## Common variants between every 2 samples and with cosmic:
@@ -80,12 +106,34 @@ def common_mutations(vcf_positions, vcf_variants, cosmicMutations):
 			if sample1 == sample2:
 				continue
 			else:
-				commonVariants[sample1][sample2] = len(vcf_positions[sample1].intersection(vcf_positions[sample2]))
+				commonVariants[sample1][sample2] = len(vcf_variants[sample1].intersection(vcf_variants[sample2]))
 		cosmicOverlap[sample1] = len(vcf_variants[sample1].intersection(cosmicMutations))
 
 	print("COMMON VARIANTS matix:")
 	print(pd.DataFrame(commonVariants).to_string())
 	print("COSMIC OVERLAP:", cosmicOverlap)
+
+	fout = open("AllCommon_PCA.txt", 'w')
+	line = ["Sample"]
+	features = ['AF', 'AC', 'DP', 'ExcessHet','FS','MQ','QD',
+	'GQ', 'DP', 'QUAL']
+	for variant in allCommon: 
+		for field in features:
+			line.append(variant + "_" + field)
+	fout.writelines("\t".join(line) + "\n")
+
+
+	for sample in samples:
+		line = [sample]
+		for variant in allCommon:
+			for field in features:
+				if field in variant_features[sample][variant]:
+					line.append(str(variant_features[sample][variant][field]))
+				else:
+					line.append('0')
+		fout.writelines("\t".join(line) + "\n")
+	fout.close()
+
 
 	return (commonVariants, cosmicOverlap)
 
@@ -122,7 +170,7 @@ def main():
 
     cosmicMutations, cosmicMutationScores = parseCosmic(args.cosmic)
     (commonVariants, cosmicOverlap) = common_mutations(vcf_positions, vcf_variants, cosmicMutations)
-    vcf_exclusives = unique_mutations(vcf_variants)
+    #vcf_exclusives = unique_mutations(vcf_variants)
 
 if __name__ == "__main__":
     main()
